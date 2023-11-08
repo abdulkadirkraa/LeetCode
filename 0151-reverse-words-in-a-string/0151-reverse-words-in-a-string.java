cd90e30a18a88df0d0f3feafd68f9a688b90ff1a
Abdulkadir <114829245+abdulkadirkraa@users.noreply.github.com>
2023-11-08 18:37:16 +0300

    Time: 8 ms (59.68%), Space: 43.9 MB (36.45%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -21,23 +21,7 @@ class Solution {
                     s1.setLength(0);
                 }
             }
-            /*
-            if(s.charAt(i)==' ' || i==0){
-                if(s1.length()==0){
-                    continue;
-                }else{
-                    //s1=
-                    s1.reverse();
-                    s1.append(" ");
-                    s2+=s1.toString();
-                    s1.setLength(0);
-                }
-            }else{
-                s1.append(s.charAt(i));
-            }
-            */
         }
         return s2.substring(0, s2.length() - 1);
     }
-
 }
